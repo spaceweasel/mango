7249df21ae783f91a080f2443de9d7368dee7acf
Aubrey Dean <aubrey.dean@gmail.com>
2016-02-16 23:57:57 +0000

    Tidy up package doc

--- doc.go
+++ doc.go
@@ -5,13 +5,18 @@
 // It uses a context per request approach which enables simplified handlers, as
 // much of the boiler plate work is done for you. The Context object takes care
 // of tasks such as serialization/deserialization, respecting the Content-Type
-// and Accept headers to ensure responses match the request.
+// and Accept headers to ensure responses match the request. You can create and
+// add your custom content-type encoders to suit your needs.
 //
 // A radix-tree based routing system enables better response times and
-// greater flexiblility in the way routes are structured and added to the system.
+// greater flexibility in the way routes are structured and added to the system.
+// Parameter based patterns with constraint rules provide great flexibility, and
+// custom rules can be added.
 //
 // Hooks and other mechanisms exist to enable customisation in accordance with
 // your specific application, such as authentication, database repository
 // injection.
+//
+// See the full documentation here https://github.com/spaceweasel/mango/wiki
 //
 package mango
